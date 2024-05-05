b39a38b23f76265e8a1934dab7766ee625cb5383
Marshal10 <marshalmaleane123@gmail.com>
2024-05-05 20:37:34 +0530

    Size selection functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container=document.querySelector('.container')
 const color1=document.querySelector('.color-1')
 const color2=document.querySelector('.color-2')
+const sizes=document.querySelectorAll('.size span')
 
 color2.addEventListener('click',()=>{
     container.classList.add('change')
@@ -8,4 +9,17 @@ color2.addEventListener('click',()=>{
 
 color1.addEventListener('click',()=>{
     container.classList.remove('change')
+})
+
+function removeActiveClasses(){
+    sizes.forEach((size)=>{
+        size.classList.remove('active')
+    })
+}
+
+sizes.forEach((size)=>{
+    size.addEventListener('click',()=>{
+        removeActiveClasses()
+        size.classList.add('active')
+    })
 })
